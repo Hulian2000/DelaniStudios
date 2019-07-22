8be3c7fefb9b266759369497fe97a4654f227a24
Hulian2000 <prepmoringa11@gmail.com>
2019-07-22 11:38:57 +0300

    add design and development

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#icon-1").click(function(){
+     $("#design").show();
+     $("#icon-1").hide();
+    });
+});
+$(document).ready(function(){
+    $("#icon-2").click(function(){
+     $("#development").show();
+     $("#icon-2").hide();
+    });
+});    
+$(document).ready(function(){
+    $("#icon-3").click(function(){
+     $("#product").show();
+     $("#icon-3").hide();
+    });
+});
+$(document).ready(function(){
+    $("#design").click(function(){
+        $("#icon-1").show();
+        $("#design").hide();
+    });
+});
+$(document).ready(function(){
+    $("#icon-2").click(function(){
+        $("#product").show();
+        $("#icon-2").hide();
+}); 
+$(document).ready(function(){
+    $("#development").click(function(){
+        $("#icon-2").show();
+        $("#development").hide();
